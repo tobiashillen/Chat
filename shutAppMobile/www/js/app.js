1c7dc09715fc10ab628dcf426a7b04af53a611bb
Hrisitijan <hristijan.bocevski@gmail.com>
2017-06-16 16:45:10 +0200

    remake

--- shutAppMobile/www/js/app.js
+++ shutAppMobile/www/js/app.js
@@ -776,8 +776,7 @@ app.controller('LeftSideController', function ($rootScope, $location, $timeout, 
 
     $scope.searchUser = function() {
         if($rootScope.person.name) {
-            $rootScope.person.name = $rootScope.person.name.toLowerCase();
-            messageManager.getHistoricMessages($rootScope.person.name).then(function(res) {
+            messageManager.getHistoricMessages($rootScope.person.name.toLowerCase()).then(function(res) {
                 $rootScope.messages = res.data;
             });
             $scope.searchMode = false;
